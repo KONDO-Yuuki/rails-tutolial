c9df0ce5d698151e42622e40717da970d4e5bdea
kondo_yuuki <efb.mzw.yk@gmail.com>
2017-05-05 08:40:29 +0000

    5th finished

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root'static_pages#home'
   get '/about', to:'static_pages#about'
   get '/help', to:'static_pages#help'
-  get '/contact', to:'static_pages#contact'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+
 
 end
