3da5b15833a42a4bfcc0d21a29e306b7433a8bd0
kondo_yuuki <efb.mzw.yk@gmail.com>
2017-06-18 11:44:47 +0000

    merege bug fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,12 @@ class SessionsController < ApplicationController
       log_in @user
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       redirect_to @user
-      
     else
       flash.now[:danger] = 'Invalid email/password combination' 
       render 'new'
     end  
   end
+
 
   def destroy
     log_out if logged_in?
