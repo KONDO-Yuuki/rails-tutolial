0a61f9ab81bde93920aa2ad36b11c82c6696906f
KONDO-yuuki <kondo-yuuki@KONDO-yuukinoMacBook-Pro.local>
2017-06-24 16:49:24 +0900

    edit test dev clear

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,30 +2,32 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
-     @user = User.new
+    @user = User.new
   end
-  
+
   def create
-    @user = User.new(user_params) 
+    @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = 'Welcome to the Sample App!'
       redirect_to @user
     else
       render 'new'
     end
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # 更新に成功した場合を扱う。
+      flash[:success] = 'Profile updated'
+      redirect_to @user
+
     else
       render 'edit'
     end
@@ -33,9 +35,8 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
-
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
