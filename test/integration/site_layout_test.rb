fd1ff83b2e28658715a64953cc37dbc9264333f5
KONDO-yuuki <kondo-yuuki@KONDO-yuukinoMacBook-Pro.local>
2017-06-27 20:28:01 +0900

    layout test update

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,17 +1,28 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test "layout links" do
+  test 'layout links' do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', logout_path, count: 0
     get contact_path
-    assert_select "title", full_title("Contact")
+    assert_select 'title', full_title('Contact')
     get signup_path
-    assert_select "title", full_title("Sign up")
-
+    assert_select 'title', full_title('Sign up')
+    log_in_as(users(:michael))
+    get users_path
+    assert_template 'users/index'
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', users_path
   end
 end
