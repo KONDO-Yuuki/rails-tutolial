5eea0a9b06243523244bb072341108bf4c22b47d
KONDO-yuuki <kondo-yuuki@KONDO-yuukinoMacBook-Pro.local>
2017-06-29 00:06:45 +0900

    authenticated custom

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,32 +1,30 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-
-  test "invalid signup information" do
+  test 'invalid signup information' do
     get signup_path
     assert_select 'form[action="/users"]'
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
-                                         email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
+      post users_path, params: { user: { name:  '',
+                                         email: 'user@invalid',
+                                         password:              'foo',
+                                         password_confirmation: 'bar' } }
     end
     assert_template 'users/new'
-    assert_select 'div.alert-danger', "The form contains 4 errors."
+    assert_select 'div.alert-danger', 'The form contains 4 errors.'
   end
-  
-  test "valid signup information" do
+
+  test 'valid signup information' do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name:  "Example User",
-                                         email: "user@example.com",
-                                         password:              "password",
-                                         password_confirmation: "password" } }
+      post users_path, params: { user: { name:  'Example User',
+                                         email: 'user@example.com',
+                                         password:              'password',
+                                         password_confirmation: 'password' } }
     end
     follow_redirect!
-    assert_template 'users/show'
-    assert flash[:success]
-    assert is_logged_in?
+    #    assert_template 'users/show'
+    assert flash[:info]
+    #    assert is_logged_in?
   end
-  
 end
