ce1cdddd15e5d1a1f2dcb83a3962d84013f62443
KONDO-yuuki <kondo-yuuki@KONDO-yuukinoMacBook-Pro.local>
2017-07-08 16:57:59 +0900

    FIX bookposts access control  next →13.3.2 マイクロポストを作成する

--- test/controllers/bookposts_controller_test.rb
+++ test/controllers/bookposts_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class BookpostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @bookpost = bookposts(:orange)
+  end
+
+  test 'should redirect create when not logged in' do
+    assert_no_difference 'Bookpost.count' do
+      post bookposts_path, params: { bookpost: { content: 'Lorem ipsum' } }
+    end
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect destroy when not logged in' do
+    assert_no_difference 'Bookpost.count' do
+      delete bookpost_path(@bookpost)
+    end
+    assert_redirected_to login_url
+  end
 end
